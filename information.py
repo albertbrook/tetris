7d31362fecebf6aa8d723f0fffb4304eb995df92
albertbrook <3226521643@qq.com>
2019-12-23 23:20:01 +0800

    more info

--- information.py
+++ information.py
@@ -7,9 +7,20 @@ class Information(object):
         self.screen = screen
 
         self.data = None
-        self.score = 0
-        self.high_score = 0
         self.reset_data()
+        self.score = None
+        self.high_score = 0
+        self.legend = [
+            [[1, 1], [1, 1]],
+            [[1, 1, 1, 1]],
+            [[1, 1, 0], [0, 1, 1]],
+            [[0, 1, 1], [1, 1, 0]],
+            [[0, 1, 0], [1, 1, 1]],
+            [[1, 0, 0], [1, 1, 1]],
+            [[0, 0, 1], [1, 1, 1]]
+        ]
+        self.legend_score = None
+        self.reset_info()
 
         self.font = pygame.font.SysFont(None, self.settings.font_size)
 
@@ -26,17 +37,34 @@ class Information(object):
                                      ((11 + j) * self.settings.block_size + self.settings.split_line_size,
                                       (1 + i) * self.settings.block_size,
                                       self.settings.block_size, self.settings.block_size))
-        self.draw_text("score", 0)
-        self.draw_text(str(self.score), 1)
-        self.draw_text("high score", 2)
-        self.draw_text(str(self.high_score), 3)
+        for z in range(len(self.legend)):
+            for i in range(len(self.legend[z])):
+                for j in range(len(self.legend[z][i])):
+                    if self.legend[z][i][j]:
+                        pygame.draw.rect(self.screen, self.settings.split_line_color,
+                                         ((11 + j / 2) * self.settings.block_size + self.settings.split_line_size,
+                                          (9.5 + z / 2 * (3 if z != 1 else 3.5) + i / 2) * self.settings.block_size,
+                                          self.settings.block_size / 2, self.settings.block_size / 2))
+        info_list = ["score", str(self.score), "high score", str(self.high_score)]
+        for score in self.legend_score:
+            info_list.append(str(score))
+        info_list_len = len(info_list) - len(self.legend_score)
+        for i in range(len(info_list)):
+            self.draw_text(info_list[i], i, info_list_len if i >= info_list_len else False)
 
-    def draw_text(self, text, y):
+    def draw_text(self, text, y, move):
         font_image = self.font.render(text, True, self.settings.split_line_color)
         font_rect = font_image.get_rect()
         font_rect.x = 12.5 * self.settings.block_size + self.settings.split_line_size - font_rect.width / 2
-        font_rect.y = (6.5 + y) * self.settings.block_size - font_rect.height / 2
+        font_rect.y = (5.5 + y) * self.settings.block_size - font_rect.height / 2
+        if move:
+            font_rect.x += 1.5 * self.settings.block_size
+            font_rect.y += (y - move + 1) * self.settings.block_size / 2
         self.screen.blit(font_image, font_rect)
 
     def reset_data(self):
         self.data = [[0] * 4 for _ in range(4)]
+
+    def reset_info(self):
+        self.score = 0
+        self.legend_score = [0] * len(self.legend)
